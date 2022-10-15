acb1bb862812aee3f836c49a91fc261e36ab45fe
joaosalg <joaosalgueiros@hotmail.com>
2022-10-15 18:09:04 -0300

    RestTemplate alternated to FeignClient

--- hr-payroll/src/main/java/com/microservice/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/services/PaymentService.java
@@ -2,32 +2,19 @@ package com.microservice.hrpayroll.services;
 
 import com.microservice.hrpayroll.entities.Payment;
 import com.microservice.hrpayroll.entities.Worker;
+import com.microservice.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
-
-// TO INSTANTIATE A INSTANCE OF PAYMENT //
 // @Service = REGISTER THIS CLASS AS A SPRING'S COMPONENT //
 @Service
 public class PaymentService {
 
-    // "MACETE" PARA GUARDAR O VALOR DO APPLICATION.PROPERTIES QUE LINK AO HR-WORKER EM UM VARIÁVEL
-    @Value("${hr-worker.host}")
-    private String wokerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment (Long workerId, int days){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", workerId.toString());
-
-        // wokerHost + "/workers/{id}" = THE ID WILL RELATE TO THE ID ADDED IN THE MAP
-        Worker worker = restTemplate.getForObject(wokerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
 
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
