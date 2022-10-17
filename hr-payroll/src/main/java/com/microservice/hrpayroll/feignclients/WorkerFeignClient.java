d906ed765101675d9e2cee040ba4cdab93f9dde6
joaosalg <joaosalgueiros@hotmail.com>
2022-10-17 19:18:07 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/microservice/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/feignclients/WorkerFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 // @COMPONENT = MEANS THAT THIS IS A COMPONENT GERENCIADO PELO SPRING PARA INJETAR EM OUTRAS CLASSES
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient {
 
     @GetMapping(value = "{id}")
--- hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResources.java
@@ -2,7 +2,10 @@ package com.microservice.hrworker.resources;
 
 import com.microservice.hrworker.entities.Worker;
 import com.microservice.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,12 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResources {
+
+    // TO PRINT INFO INTO LOG
+    private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private WorkerRepository repository;
@@ -29,6 +38,9 @@ public class WorkerResources {
     // @PathVariable = RECOGNIZES Long id w/ MAPPED id "/{id}"
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        // TO PRINT THE NUMBER OF THE PORT THAT THE INSTANCE IS RUNNING
+        logger.info("Port = " + env.getProperty("local.server.port"));
+
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
